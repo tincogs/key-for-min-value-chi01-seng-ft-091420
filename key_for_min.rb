23ec10e075a89ffd5a233fb3158f489712d6975c
Tom ODay <tomoday91@gmail.com>
2020-09-08 18:01:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,12 @@
 
 def key_for_min_value(name_hash)
 minimum = 1000
-min_item = ""
+min_item = nil
 name_hash.each do |item, amount|
   if amount < minimum
     minimum =  amount
     min_item = item
   end
 end
-if min_item.is_a? Symbol
   min_item
-else nil
-end
 end
