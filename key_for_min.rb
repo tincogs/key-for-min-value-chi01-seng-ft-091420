f9e5ff0dd69abe9223dfd4ff2cc73ba328e5e4f8
Tom ODay <tomoday91@gmail.com>
2020-09-08 17:46:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+minimum = 1000
+min_item = ""
+name_hash.each do |item, amount|
+  if amount < minimum
+    min_item = item
+  end
+end
+min_item
 end
